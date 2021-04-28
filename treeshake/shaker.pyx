c3c6dbacb86198caf8bca2b78c6536060445dd94
j.veldhuis <jobbfveldhuis@gmail.com>
2021-04-28 20:45:28 +0200

    docs: Added docstrings for shaker.pyx.

--- treeshake/shaker.pyx
+++ treeshake/shaker.pyx
@@ -6,6 +6,13 @@ from treeshake.stylesheet import Stylesheet
 
 
 cdef class Shaker:
+    """
+    Tree shaker class that holds all information needed for comparison.
+
+    Attributes:
+        _stylesheets (list): A list of paths to .css files that will be used in comparison.
+        _html_files (list): A list of paths to .html files that will be used in comparison.
+    """
     cdef list _stylesheets, _html_files
 
     def __init__(self):
@@ -13,16 +20,38 @@ cdef class Shaker:
         self._html_files = []
 
     cpdef void add_stylesheet(self, str path) except *:
+        """Adds a .css file to the list of stylesheets.
+        
+        Arguments:
+            path (str): The path to the css file to add to the list.
+        """
         self._stylesheets.append(path)
 
     cpdef void add_html_file(self, str path) except *:
+        """Adds a .html file to the list of html files.
+        
+        Arguments:
+            path (str): The path to the html file to add to the list.
+        """
         self._html_files.append(path)
 
     cpdef void discover_add_stylesheets(self, str path, bint recursive=False) except *:
+        """Finds all css files in a certain path and adds them to the list.
+        
+        Arguments:
+            path (str): The path to the directory to use to discover.
+            recursive (bool): Whether or not to go in-depth and find files recursively.
+        """
         for stylesheet in self.discover(path, '.css', recursive):
             self.add_stylesheet(stylesheet)
 
     cpdef void discover_add_html(self, str path, bint recursive = False) except *:
+        """Finds all html files in a certain path and adds them to the list.
+
+        Arguments:
+            path (str): The path to the directory to use to discover.
+            recursive (bool): Whether or not to go in-depth and find files recursively.
+        """
         for html_file in self.discover(path, '.html', recursive):
             self.add_html_file(html_file)
 
@@ -31,6 +60,13 @@ cdef class Shaker:
         print(self._html_files)
 
     cpdef list discover(self, str path, str extension, bint recursive=False):
+        """Discovers files in a certain folder (recursively).
+        
+        Arguments:
+            path (str): The path to the directory that is used as a starting point.
+            extension (str): The extension to search for.
+            recursive (bool): Whether or not to go in-depth and find files recursively.
+        """
         cpdef list results = []
         if recursive:
             for root, dirs, files in os.walk(path):
@@ -48,6 +84,14 @@ cdef class Shaker:
         return results
 
     cpdef void optimize(self, str output_directory):
+        """Tree-shakes all css files in self._stylesheets.
+        
+        Compares all rules in the css-file to the html files and, if the rule is not used in any html file,
+        deletes it from the output file. This leaves only used css-rules in the final product.
+        
+        Arguments:
+            output_directory (str): The directory to output the files to.
+        """
         if output_directory.endswith('/'):
             output_directory = output_directory[:-1]
 
@@ -61,4 +105,3 @@ cdef class Shaker:
             file_name = ntpath.basename(stylesheet)
             obj = Stylesheet(stylesheet)
             obj.optimize(self._html_files, f'{output_directory}/{file_name}')
-
