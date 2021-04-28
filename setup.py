0f4bbbf649578e34c970722cdda79f902864f13a
j.veldhuis <jobbfveldhuis@gmail.com>
2021-04-28 21:41:16 +0200

    build: Move away from Poetry to setuptools.

--- setup.py
+++ setup.py
@@ -1,7 +1,20 @@
-import setuptools
+from setuptools import setup, find_packages
 from Cython.Build import cythonize
 
-setuptools.setup(
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+setup(
     name='treeshake',
+    version='0.1.4',
+    packages=find_packages(),
+    description='Remove unused css in Python projects with C-powered tree shaking.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/jackmanapp/treeshake/',
     ext_modules=cythonize('treeshake/*.pyx', compiler_directives={'language_level': "3"}),
+    install_requires=[
+        'beautifulsoup4>=4.9.3',
+        'cssutils>=2.2.0'
+    ]
 )
