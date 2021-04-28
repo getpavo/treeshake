f77db93076e54d33383af0cc770bfdf6d1259ca5
j.veldhuis <jobbfveldhuis@gmail.com>
2021-04-28 20:49:41 +0200

    docs: Added docstrings for stylesheet.pyx.

--- treeshake/stylesheet.pyx
+++ treeshake/stylesheet.pyx
@@ -3,12 +3,26 @@ from bs4 import BeautifulSoup
 
 
 cdef class Stylesheet:
+    """Class that holds logic for a single Stylesheet.
+
+    Attributes:
+        path (str): The path to the file that the Stylesheet class belongs to.
+    """
     cdef str path
 
     def __init__(self, str path):
         self.path = path
 
     cpdef bint compare_with_html(self, str selector, str html_path) except *:
+        """Checks whether the given selector is used in a single html file.
+        
+        Arguments:
+            selector (str): The css-selector to find in the html document.
+            html_path (str): The path to the html file to use when comparing.
+            
+        Returns:
+            bool: Whether or not the selector was found in the given html file.
+        """
         with open(html_path, 'r') as f:
             soup = BeautifulSoup(f, 'html.parser')
 
@@ -18,6 +32,16 @@ cdef class Stylesheet:
         return False
 
     cpdef void optimize(self, list html_files, str output_path=None) except *:
+        """Optimizes the stylesheet and outputs the content to a specified path.
+        
+        Parses the css and finds all selectors in the parsed document. Then, continues to iterate over all given 
+        html files and checks whether or not the selector is used in these documents. If it used at least once,
+        the css-rule will be added to the new stylesheet, which is outputted to the output_path.
+        
+        Arguments:
+            html_files (list): List of paths to html files to compare.
+            output_path (str): The path to the output directory. If none, defaults to original css folder.
+        """
         if output_path is None:
             output_path = self.path
 
