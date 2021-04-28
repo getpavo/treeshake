a1c2f9e5e47ca14735b4f6e8681c0e1042f9f635
j.veldhuis <jobbfveldhuis@gmail.com>
2021-04-28 23:33:52 +0200

    build: Bump version to 0.2.0 pending release.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", 'r') as f:
 
 setup(
     name='treeshake',
-    version='0.1.5',
+    version='0.2.0',
     packages=find_packages(),
     description='Remove unused css in Python projects with C-powered tree shaking.',
     long_description=long_description,
