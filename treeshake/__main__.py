d1d0b191fd6772ac6dea38746fbca5fdda46833e
j.veldhuis <jobbfveldhuis@gmail.com>
2021-04-29 00:12:05 +0200

    feat: Gracefully handle KeyboardInterrupt.

--- treeshake/__main__.py
+++ treeshake/__main__.py
@@ -6,32 +6,36 @@ from treeshake.shaker import Shaker
 
 
 def main(args=None):
-    init()
+    try:
+        init()
 
-    if args is None:
-        args = argv[1:]
+        if args is None:
+            args = argv[1:]
 
-    css_folder = args[0] if len(args) > 0 else None
-    html_folder = args[1] if len(args) > 1 else None
-    output_folder = args[2] if len(args) > 2 else None
+        css_folder = args[0] if len(args) > 0 else None
+        html_folder = args[1] if len(args) > 1 else None
+        output_folder = args[2] if len(args) > 2 else None
 
-    if css_folder is None:
-        css_folder = _ask('CSS directory path')
+        if css_folder is None:
+            css_folder = _ask('CSS directory path')
 
-    if html_folder is None:
-        html_folder = _ask('HTML directory path')
+        if html_folder is None:
+            html_folder = _ask('HTML directory path')
 
-    if output_folder is None:
-        output_folder = _ask('Output directory path')
+        if output_folder is None:
+            output_folder = _ask('Output directory path')
 
-    treeshaker = Shaker()
-    treeshaker.discover_add_stylesheets(css_folder)
-    treeshaker.discover_add_html(html_folder)
-    start_time = now()
+        treeshaker = Shaker()
+        treeshaker.discover_add_stylesheets(css_folder)
+        treeshaker.discover_add_html(html_folder)
+        start_time = now()
 
-    files = treeshaker.optimize(output_folder)
+        files = treeshaker.optimize(output_folder)
 
-    print(f'{Fore.GREEN}\nDone! Optimized {files} files in {round(now() - start_time, 5)} seconds!\n{Style.RESET_ALL}')
+        print(f'{Fore.GREEN}\nDone! Optimized {files} files in {round(now() - start_time, 5)} seconds!\n{Style.RESET_ALL}')
+    except KeyboardInterrupt:
+        print(f'{Fore.BLUE}\nDetected hard interrupt. Bye bye!\n{Style.RESET_ALL}')
+        exit()
 
 
 def _ask(msg):
