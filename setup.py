d24849f0490880b854d574e43e8f7b73d5bcf530
j.veldhuis <jobbfveldhuis@gmail.com>
2021-04-28 22:06:29 +0200

    build: Add classifiers to setup.

--- setup.py
+++ setup.py
@@ -11,6 +11,11 @@ setup(
     description='Remove unused css in Python projects with C-powered tree shaking.',
     long_description=long_description,
     long_description_content_type='text/markdown',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Utilities'
+    ],
     url='https://github.com/jackmanapp/treeshake/',
     ext_modules=cythonize('treeshake/*.pyx', compiler_directives={'language_level': "3"}),
     install_requires=[
