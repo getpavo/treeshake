b25390f4b067054ccca0b43ced9d39efdf2dd5ea
j.veldhuis <jobbfveldhuis@gmail.com>
2021-05-02 14:14:13 +0200

    feat: Add argument parsing to __main__.py

--- treeshake/__main__.py
+++ treeshake/__main__.py
@@ -1,4 +1,5 @@
 from sys import argv
+from argparse import ArgumentParser
 from colorama import init, Fore, Style
 from time import time as now
 
@@ -8,13 +9,15 @@ from treeshake.shaker import Shaker
 def main(args=None):
     try:
         init()
-
+        parser = _create_parser()
         if args is None:
             args = argv[1:]
 
-        css_folder = args[0] if len(args) > 0 else None
-        html_folder = args[1] if len(args) > 1 else None
-        output_folder = args[2] if len(args) > 2 else None
+        args = parser.parse_args(args)
+
+        css_folder = args.css
+        html_folder = args.html
+        output_folder = args.css if args.overwrite is True else args.out
 
         if css_folder is None:
             css_folder = _ask('CSS directory path')
@@ -40,6 +43,19 @@ def main(args=None):
 
 def _ask(msg):
     return input(f'{Fore.YELLOW}\n> {msg}: {Style.RESET_ALL}')
+
+
+def _create_parser():
+    parser = ArgumentParser(description='Tree shake stylesheets and improve performance from command line.')
+    parser.add_argument('css', help='The path to the css folder', type=str)
+    parser.add_argument('html', help='The path to the HTML folder', type=str)
+
+    # Mutually exclusive group for output file information
+    output_group = parser.add_mutually_exclusive_group(required=False)
+    output_group.add_argument('--out', default='./out/', type=str)
+    output_group.add_argument('--overwrite', default=False, action='store_true')
+
+    return parser
 
 
 if __name__ == '__main__':
