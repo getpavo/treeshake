f491a489ac9f7bfadcd58cce7a9734c348dc5d38
j.veldhuis <jobbfveldhuis@gmail.com>
2021-08-09 14:22:25 +0200

    chore: Remove test.py file from VC.

--- treeshake/test.py
+++
@@ -1,6 +0,0 @@
-from treeshake.shaker import Shaker
-
-treeshaker = Shaker()
-treeshaker.discover_add_stylesheets('./src/', True)
-treeshaker.discover_add_html('./html/')
-treeshaker.optimize('./out')
